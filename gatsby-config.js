62166adaa17849bc84c8703a85aeb7a04127dd86
souheil <souhail@MBP-de-souheil.home>
2021-12-29 02:20:26 +0100

    message

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Souheil`,
+    siteTitleAlt: `Souheil Bouzouita`,
+    siteHeadline: `Souheil Bouzouita `,
+    siteUrl: `https://SouheilBouzouita.com`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Souheil`,
   },
   plugins: [
     {
